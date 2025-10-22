fc4ebe20c554e23040a10f6443c94a4658c66409
Ctrl-Nova <abhinavpro.gup8@gmail.com>
2025-10-22 11:52:28 +0000

    Datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";// treats all JS code as the newer version
+//alert(3+3) // we are using nodeJS not browser
+
+console.log(3
+    +
+    3)//the code readability should be high.
+
+console.log("Abhinav")
+
+let name = "Abhi"
+let age = 17
+let isLoggedIn = true
+
+/*
+Number ==> 2 to the power of 53
+bigint 
+String ==> ""
+boolean ==> true/false
+null ==> representation of a khali value(empty)| let state = null;
+undefined ==> kuch value hi nahi hai| let state;
+symbol ==> to find uniqueness of something 
+object
+*/
+
+console.log(typeof "Abhinav")
+console.log(typeof age)
+let state = null;
+console.log(typeof state);//output is object
+let country;
+console.log(typeof country);//output is undefined
