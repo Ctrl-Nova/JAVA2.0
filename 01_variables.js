8df8c7e6ac3757131ea126e6f704dfafa62bbb6a
Ctrl-Nova <abhinavpro.gup8@gmail.com>
2025-10-22 11:52:18 +0000

    Variables

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 123
+let accountEmail = "ab@ggl.com"
+var accountPass = "45"
+accountCity = "Jaipur"
+
+// accountID = 2
+// console.log(accountID);
+accountEmail = "hc@hc.com"
+accountPass = 212
+accountCity = "delhi"
+console.table([accountID,accountEmail,accountPass,accountCity]);
+/*
+dont use "var" as it poses an issue to the block scope and functional scope
+*/ 
+
